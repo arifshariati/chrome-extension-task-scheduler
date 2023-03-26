a4a897abf63b018f591b2244395d2a352afb37da
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-03-26 17:00:18 +0200

    fixed axios usage with fetch

--- ext/src/services/backend.ts
+++ ext/src/services/backend.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
 export const makeApiCall = async () => {
   try {
-    await axios.get("http://localhost:3001");
+    await fetch("http://localhost:3001");
   } catch (error: any) {
     console.log(error.message);
   }

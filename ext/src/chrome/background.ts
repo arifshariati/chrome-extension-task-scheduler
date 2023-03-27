71f7cc9ad8f6567d1568d900ed8d2f3728ff2ea9
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-03-27 10:56:17 +0200

    Added stop trigger catch in background.js

--- ext/src/chrome/background.ts
+++ ext/src/chrome/background.ts
@@ -1,5 +1,6 @@
 const allowedTarget = "chrome-extension";
 const backendBaseUrl = "http://localhost:3001";
+const stopTasks = "STOP";
 
 const makeApiCall = async (): Promise<void> => {
   try {
@@ -13,6 +14,11 @@ const makeApiCall = async (): Promise<void> => {
 };
 
 chrome.runtime.onMessage.addListener((request: any, sender: chrome.runtime.MessageSender, sendResponse: (response: any) => void) => {
+  if (request?.payload?.actionType === stopTasks) {
+    console.log(request.payload);
+    console.log("STOP tasks triggerd");
+    return;
+  }
   if (request.target === allowedTarget) {
     makeApiCall();
   }
